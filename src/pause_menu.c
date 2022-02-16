63ba13caf77a72b448799620b53382cb64b6ee1d
Aillet <arthur.aillet@epitech.eu>
2022-02-16 16:52:02 +0100

    [ADD] new button

--- src/pause_menu.c
+++ src/pause_menu.c
@@ -29,6 +29,9 @@ int get_pause_event(sfRenderWindow *window, char *keys)
 
 void pause_menu(sfRenderWindow *window, char *keys)
 {
+    object button1 = create_object("sprites/Rectangle_button.png", VC{0.2, 0.2}, VC{1920/2 - (2000*0.2)/2, 1080/2 - (800*0.2)*1.2});
+    object button2 = create_object("sprites/Rectangle_button.png", VC{0.2, 0.2}, VC{1920/2 - (2000*0.2)/2, 1080/2});
+    object button3 = create_object("sprites/Rectangle_button.png", VC{0.2, 0.2}, VC{1920/2 - (2000*0.2)/2, 1080/2 + (800*0.2)*1.2});
     object pause = create_object("sprites/pause_menu.png", VC{1, 1}, VC{0, 0});
     object bg = create_object("sprites/ye_olde_map.png", VC{5, 5}, VC{90, 0});
     sfSprite *mouse = setup_mouse();
@@ -38,10 +41,16 @@ void pause_menu(sfRenderWindow *window, char *keys)
         sfRenderWindow_clear(window, sfBlack);
         sfRenderWindow_drawSprite(window, bg.sprite, NULL);
         sfRenderWindow_drawSprite(window, pause.sprite, NULL);
+        sfRenderWindow_drawSprite(window, button1.sprite, NULL);
+        sfRenderWindow_drawSprite(window, button2.sprite, NULL);
+        sfRenderWindow_drawSprite(window, button3.sprite, NULL);
         close = get_pause_event(window, keys);
         draw_mouse(window, mouse);
         sfRenderWindow_display(window);
     }
+    destroy_object(button1);
+    destroy_object(button2);
+    destroy_object(button3);
     destroy_object(pause);
     destroy_object(bg);
 }
